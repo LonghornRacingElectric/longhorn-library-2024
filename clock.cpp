91238399094f96fd222e240c99624a6f262a20b8
matt-mekha <matthew.mekha@gmail.com>
2023-12-03 15:46:51 -0600

    clock

---
+++ clock.cpp
@@ -0,0 +1,17 @@
+#include "clock.h"
+
+static uint32_t lastTimeRecorded = 0;
+
+void clock_init() {
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(SysTick_IRQn);
+}
+
+float clock_getDeltaTime() {
+    uint32_t time = HAL_GetTick();
+    float deltaTime = ((float)(time - lastTimeRecorded)) / 1000.0f;
+    lastTimeRecorded = time;
+    return deltaTime;
+}
---
+++ clock.h
@@ -0,0 +1,10 @@
+#ifndef LONGHORN_LIBRARY_CLOCK_H
+#define LONGHORN_LIBRARY_CLOCK_H
+
+#include <stdint.h>
+#include "main.h"
+
+void clock_init();
+float clock_getDeltaTime();
+
+#endif //LONGHORN_LIBRARY_CLOCK_H
