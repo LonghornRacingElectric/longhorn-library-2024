37f6022d570d6086b383f8af4727d7ffe4e1b788
Yash Mittal <yashkmittal@utexas.edu>
2023-12-18 21:54:25 -0600

    improv: added timer support for CAN packets, which allows for rate-limiting, reducing clog on bus
    * Added two hashmaps that store the period and curr_time of each ID
    
    * Functions related to timers are similar to mailbox functions
    
    fix: can_send supports data lengths from 1 - 8 bytes

--- angel_can.cpp
+++ angel_can.cpp
@@ -6,7 +6,30 @@
 using namespace std;
 
 static unordered_map<uint32_t, CanRx *> can_mailboxes;
+static unordered_map<uint32_t, uint32_t> can_periodic_timers;
+static unordered_map<uint32_t, uint32_t> can_periodic_curr_time;
 static CAN_HANDLE *canHandleTypeDef;
+
+/**
+ * Get the period associated with the ID
+ * @param id ID of the CAN packet.
+ * @return Period of the CAN packet in milliseconds, returns 0 if it doesn't exist
+ */
+static uint32_t can_getTimer(uint32_t id);
+
+/**
+ * Get the mailbox associated with the ID
+ * @param id ID of the CAN packet.
+ * @return Pointer to the associated mailbox, returns null if it doesn't exist
+ */
+static CanRx* can_getMailbox(uint32_t id);
+
+/**
+ * Get the current time associated with the ID
+ * @param id ID of the CAN packet.
+ * @return Current time of the CAN packet in milliseconds, returns 0 if it doesn't exist
+ */
+static uint32_t can_getCurrTime(uint32_t id);
 
 #ifdef STM32H7A3xx
 uint8_t dlc_to_num(uint32_t dlc) {
@@ -70,6 +93,20 @@ uint32_t can_init(CAN_HANDLE *handle) {
     return HAL_CAN_Start(canHandleTypeDef);
 #endif
 }
+void can_addTimer(uint32_t id, uint32_t period) {
+    if (can_periodic_timers.find(id) != can_periodic_timers.end()) {
+        return;
+    }
+
+    can_periodic_timers.insert({id, period});
+    can_periodic_curr_time.insert({id, 0});
+}
+
+void can_addRangeTimers(uint32_t idLow, uint32_t idHigh, uint32_t period) {
+    for(uint32_t i = idLow; i <= idHigh; i++) {
+        can_addTimer(i, period);
+    }
+}
 
 void can_addMailbox(uint32_t id, CanRx *mailbox) {
     if (can_mailboxes.find(id) != can_mailboxes.end()) {
@@ -82,6 +119,22 @@ void can_addMailbox(uint32_t id, CanRx *mailbox) {
 void can_addMailboxes(uint32_t idLow, uint32_t idHigh, CanRx *mailboxes) {
     for(uint32_t i = idLow; i <= idHigh; i++) {
         can_addMailbox(i, &mailboxes[i - idLow]);
+    }
+}
+
+static uint32_t can_getCurrTime(uint32_t id) {
+    if (can_periodic_curr_time.find(id) == can_periodic_curr_time.end()) {
+        return 0;
+    } else {
+        return can_periodic_curr_time.at(id);
+    }
+}
+
+static uint32_t can_getTimer(uint32_t id) {
+    if (can_periodic_timers.find(id) == can_periodic_timers.end()) {
+        return 0;
+    } else {
+        return can_periodic_timers.at(id);
     }
 }
 
@@ -140,7 +193,7 @@ void can_clearMailboxes() {
     }
 }
 
-uint32_t can_send(uint32_t id, uint8_t dlc, uint8_t data[8]) {
+uint32_t can_send(uint32_t id, uint8_t dlc, uint8_t* data) {
 #ifdef STM32H7A3xx
     static FDCAN_TxHeaderTypeDef TxHeader;
     TxHeader.Identifier = id;
@@ -176,6 +229,20 @@ uint32_t can_send(uint32_t id, uint8_t dlc, uint8_t data[8]) {
 #endif
 
     return 0;
+}
+
+uint32_t can_send_periodic(uint32_t id, uint8_t dlc, uint8_t data[8], uint32_t delta){
+    uint32_t curr_time = can_getCurrTime(id);
+    uint32_t timer = can_getTimer(id);
+    if (timer == 0) { // No timer set
+        return can_send(id, dlc, data);
+    } else if (curr_time + delta >= timer) { // Timer expired
+        can_periodic_curr_time[id] = 0;
+        return can_send(id, dlc, data);
+    } else { // Timer not expired
+        can_periodic_curr_time[id] += delta;
+        return 0;
+    }
 }
 
 uint64_t can_readBytes(const uint8_t data[8], uint8_t start_byte, uint8_t end_byte) {
--- angel_can.h
+++ angel_can.h
@@ -68,27 +68,37 @@ typedef struct CanRx {
 uint32_t can_init(CAN_HANDLE* handle);
 
 /**
- * Tell the CAN driver to copy all incoming packets with a given ID to the given mailbox address.
- * Mailbox must be pre-defined
+ * Tell the CAN driver to associate every ID with a period (in ms).\n
+ * The period is the maximum rate at which CAN packets of this ID can be pushed to the FIFO. \n
+ * @param id ID of the CAN packet you want to add
+ * @param period Period of the CAN packet in milliseconds
+ */
+void can_addTimer(uint32_t id, uint32_t period);
+
+/**
+ * Tell the CAN driver to associate the range of IDs with a period (in ms).\n
+ * The period is the maximum rate at which CAN packets of this ID can be pushed to the FIFO. \n
+ * @param idLow ID of the CAN packet you want to add
+ * @param idHigh ID of the CAN packet you want to add
+ * @param period Period of the CAN packet in milliseconds
+ */
+void can_addRangeTimers(uint32_t idLow, uint32_t idHigh, uint32_t period);
+
+/**
+ * Tell the CAN driver to copy all incoming packets with a given ID to the given mailbox address.\n
+ * Mailbox must be pre-defined (i.e., declared in your code as "static CanRx <name>") .\n
  * @param id ID of the CAN packet you want to add
  * @param mailbox Pointer to where the incoming packet is stored.
  */
 void can_addMailbox(uint32_t id, CanRx* mailbox);
 
 /**
- * Tell the CAN driver to copy all incoming packets with a given ID range to the given mailbox array.
- * Mailbox is a pointer to an array of CanRx where the first element corresponds to the first ID in the range.
+ * Tell the CAN driver to copy all incoming packets with a given ID range to the given mailbox array.\n
+ * Mailbox is a pointer to an array of CanRx where the first element corresponds to the first ID in the range.\n
  * @param id ID of the CAN packet you want to add
  * @param mailbox Pointer to where the incoming packet is stored.
  */
 void can_addMailboxes(uint32_t idLow, uint32_t idHigh, CanRx* mailboxes);
-
-/**
- * Get the mailbox associated with the ID
- * @param id ID of the CAN packet.
- * @return Pointer to the associated mailbox, returns null if it doesn't exist
- */
-static CanRx* can_getMailbox(uint32_t id);
 
 /**
  * Update the corresponding mailboxes, emptying the RxFifo.
@@ -101,19 +111,44 @@ uint32_t can_processRxFifo();
 void can_clearMailboxes();
 
 /**
- * Put a CAN packet in the Tx FIFO.
+ * Put a CAN packet in the Tx FIFO, which is guaranteed to be pushed onto the CAN BUS
+ * @param id ID of the CAN packet
+ * @param dlc Length of the CAN packet
+ * @param data Data of the CAN packet
+ * @return 0 if successful, 1 if unsuccessful
  */
-uint32_t can_send(uint32_t id, uint8_t dlc, uint8_t data[8]);
+uint32_t can_send(uint32_t id, uint8_t dlc, uint8_t* data);
 
 /**
- * Read a parameter from the packet, specifying which bytes to read.
- * This assumes that the packets are little endian, which is the standard set in CM200dz
+ * Attempts to put in a CAN packet in the Tx FIFO so that it is rate-limited.\n
+ * The period of that rate is pre-determined by the ID through can_addTimer or can_addRangeTimers. \n
+ * If no rate is set for the ID, the packet is sent without rate-limiting.\n
+ * @param id ID of the CAN packet
+ * @param dlc Length of the CAN packet
+ * @param data Data of the CAN packet
+ * @param delta Time in milliseconds between each packet
+ * @return 0 if successful, 1 if error, 2 if unsuccessful send
+ */
+uint32_t can_send_periodic(uint32_t id, uint8_t dlc, uint8_t data[8], uint32_t delta);
+
+/**
+ * Read a parameter from the packet, specifying which bytes to read.\n
+ * This assumes that the packets are little endian, which is the standard set in CM200dz.\n
+ * @param data Data of the CAN packet
+ * @param start_byte First byte to read
+ * @param end_byte Last byte to read
+ * @return Value of the parameter
+ * @note This supports up to 64 bit integers, and the result must be cast into other types by you.
  */
 uint64_t can_readBytes(const uint8_t data[8], uint8_t start_byte, uint8_t end_byte);
 
 /**
- * Write a parameter to the packet, specifying which bytes to write.
- * This assumes that the packets are little endian, which is the standard set in CM200dz
+ * Write a parameter to the packet, specifying which bytes to write.\n
+ * This assumes that the packets are little endian, which is the standard set in CM200dz.\n
+ * @param data Data of the CAN packet
+ * @param start_byte First byte to write
+ * @param end_byte Last byte to write
+ * @param value Value of the parameter
  */
 void can_writeBytes(uint8_t data[8], uint8_t start_byte, uint8_t end_byte, uint64_t value);
 
