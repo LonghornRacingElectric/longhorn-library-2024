d32034b2bf530df7dcfe7c6f8572d71970aa3d52
estherrliu <liuesthers@gmail.com>
2024-02-11 14:30:57 -0600

    hspi edits

--- imu.c
+++ imu.c
@@ -46,8 +46,8 @@ static void imu_scale (){
 //GYRO 208Hz high performance, 4000dps
 #define CTRL2_G_REG 0b00001011
 #define CTRL2_G_VAL 0b01010001
-void imu_init(uint8_t hspi_number) {
-    static SPI_HandleTypeDef *hspi = &hspi_number;
+void imu_init(SPI_HandleTypeDef hspi_ptr) {
+    static SPI_HandleTypeDef *hspi = &hspi_ptr;
     imu_writeregister1(CTRL1_XL_REG, CTRL1_XL_VAL);
     imu_writeregister1(CTRL2_G_REG, CTRL2_G_VAL);
     imu_calibrate();
